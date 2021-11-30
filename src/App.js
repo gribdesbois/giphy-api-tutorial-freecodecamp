350f0ae6d4c5ef9c9772e41933bf79e322563714
gribdesbois <gribdesbois@gmail.com>
2021-11-30 13:59:30 +0100

    imported _ from 'dotenv/config' in a separate files to have it working.. way easier than react-dotenv

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import _ from './env.js'
+import { GiphyFetch } from '@giphy/js-fetch-api'
+import { useState } from 'react'
+import TextList from './components/TextList' 
+import Error from './components/Error'
+import './App.css'
+/* dotenv.config() */
+
+
+const {REACT_APP_GIPHY_KEY} = process.env
+const giphy = new GiphyFetch(REACT_APP_GIPHY_KEY)
 
 function App() {
+  const [text, setText] = useState('')
+  const [results, setResults] = useState([])
+  const [err, setErr] = useState(false)
+
+  const handleInput = (event) => {
+    setText(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    if (text.length === 0) {
+      //set error state to true
+      setErr(true)
+      return
+    }
+    console.log(text)
+
+    const apiCall = async () => {
+      const res = await giphy.animate(text, {limit: 20})
+      console.log(res.data)
+      setResults(res.data)
+    }
+
+    apiCall()
+    setText('')
+    setErr(false)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Animated Text Generator</h1>
+      <h3>Type text into the form and hit submit</h3>
+      <input className='input-field' value={text} onChange={handleInput} type='text' />
+      <button className='submit-btn' onClick={handleSubmit}>Submit</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/TextList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TextList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TextList
